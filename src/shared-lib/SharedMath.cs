941082b7abcaef9d48f2376657fbc1a423650281
Kamran Ayub <kamran@hey.com>
2025-10-06 11:25:56 -0500

    add xml docs

--- src/shared-lib/SharedMath.cs
+++ src/shared-lib/SharedMath.cs
@@ -2,8 +2,17 @@ using Microsoft.JavaScript.NodeApi;
 
 namespace SharedLib;
 
+/// <summary>
+/// Shared .NET math utilities
+/// </summary>
 [JSExport]
 public static class SharedMath
 {
+    /// <summary>
+    /// Adds two numbers together
+    /// </summary>
+    /// <param name="a">The first number</param>
+    /// <param name="b">The second number</param>
+    /// <returns>The added number</returns>
     public static int Add(int a, int b) => a + b;
 }
